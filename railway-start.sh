a8b95f38f7f82144d782ed58f783eeaf172317d0
Akshansh Business <akshansh.d.business@gmail.com>
2026-02-20 13:52:41 +0530

    fixredeploy3

--- railway-start.sh
+++ railway-start.sh
@@ -3,15 +3,14 @@ set -e
 
 APP_DIR=/var/www/html
 
-# If not installed (config missing)
 if [ ! -f "$APP_DIR/data/config.php" ]; then
     echo "First run: copying EspoCRM files into volume..."
-
     cp -r /usr/src/espo/* $APP_DIR/
-
     chown -R www-data:www-data $APP_DIR
     chmod -R 775 $APP_DIR
+else
+    echo "Existing installation detected"
 fi
 
 echo "Starting Apache..."
-apache2-foreground
+exec apache2-foreground
